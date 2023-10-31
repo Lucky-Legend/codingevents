57e27650f54522bad7fb585bce577f3113af2974
Timothy Nicolas <timnicolas87@gmail.com>
2023-10-31 00:18:08 -0500

    added form handler method

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -11,13 +13,10 @@ import java.util.ArrayList;
 @RequestMapping("events")
 public class EventController {
 
+    private static ArrayList<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        ArrayList<String> events = new ArrayList<>();
-        events.add("Christmas");
-        events.add("All Saints Day");
-        events.add("Easter");
-        events.add("Immaculate Conception");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -26,5 +25,12 @@ public class EventController {
     public String renderCreateEventForm() {
         return "events/create";
     }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
+    }
+
 
 }
